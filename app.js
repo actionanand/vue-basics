a7c6b427ebc3212a7de8c4288f8bbe93ff111bbe
Anand raja <46064269+actionanand@users.noreply.github.com>
2022-01-09 23:26:11 +0530

    dynamic styles - final

--- app.js
+++ app.js
@@ -4,16 +4,27 @@ const app = Vue.createApp({
       boxASelected: false,
       boxBSelected: false,
       boxCSelected: false,
+      boxDSelected: false,
+      boxESelected: false
     };
+  },
+  computed: {
+    boxDClasses() {
+      return { active: this.boxDSelected };
+    }
   },
   methods: {
     boxSelected(box) {
       if(box === 'A') {
         this.boxASelected = !this.boxASelected;
-      } else if (box === 'B') {
+      } else if(box === 'B') {
         this.boxBSelected = !this.boxBSelected;
-      } else if (box === 'C') {
+      } else if(box === 'C') {
         this.boxCSelected = !this.boxCSelected;
+      } else if(box === 'D') {
+        this.boxDSelected = !this.boxDSelected;
+      } else if(box === 'E') {
+        this.boxESelected = !this.boxESelected;
       }
     }
   }
