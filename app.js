683b4f09e60086ea709be129ad43203aee13d873
Anand raja <46064269+actionanand@users.noreply.github.com>
2022-01-09 22:59:46 +0530

    binding style property dynamically

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('Hi');
+const app = Vue.createApp({
+  data() {
+    return {
+      boxASelected: false,
+      boxBSelected: false,
+      boxCSelected: false,
+    };
+  },
+  methods: {
+    boxSelected(box) {
+      if(box === 'A') {
+        this.boxASelected = true;
+      } else if (box === 'B') {
+        this.boxBSelected = true;
+      } else if (box === 'C') {
+        this.boxCSelected = true;
+      }
+    }
+  }
+});
+
+app.mount('#styling');
