09099a0c595699e60b000c646b614ba718c13aa1
Anand raja <46064269+actionanand@users.noreply.github.com>
2022-01-09 19:59:15 +0530

    counter example

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoalA: 'Finish the course and learn Vue!',
-      courseGoalB: '<h3>Master Vue and build amazing apps!</h3>',
-      vueLink: 'https://v3.vuejs.org/'
+      counter: 0,
     };
   },
-  methods: {
-    outputGoals() {
-      const randomNum = Math.random();
-      if(randomNum > .5) {
-        return this.courseGoalB;
-      } else {
-        return this.courseGoalA;
-      }
-    }
-  }
 });
 
-app.mount('#user-goal');
-
+app.mount('#events');
