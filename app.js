ca4482e4a1a2fb1c910baced48499cc493ed6c54
Anand raja <46064269+actionanand@users.noreply.github.com>
2022-01-09 20:53:23 +0530

    prevent modifier

--- app.js
+++ app.js
@@ -15,8 +15,7 @@ const app = Vue.createApp({
     setName(event) {
       this.name = event.target.value;
     },
-    submitForm(event) {
-      event.preventDefault();
+    submitForm() {
       alert('Submited!');
     }
   }
